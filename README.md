64d2ea281c1d05b1550da4363846db52378a7be0
kurabito <jeffjames@gmail.com>
2021-07-28 20:35:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# clock-vue
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/clock-vue)
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app" class="vertical-center">
+    <Clock/>
+  </div>
+</template>
+
+<script>
+import Clock from './components/Clock.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Clock
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Arial Black, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  font-size: 350%;
+}
+.vertical-center {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+</style>
